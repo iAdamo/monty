cc21c0df5ea383bf2e3a46e9be65bd86f7fbdd40
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-08-25 21:40:32 +0100

    pop opcode

--- 00.m
+++ 00.m
@@ -1,9 +1,10 @@
-pall
 push 1
-pall
 push 2
-pall
 push 3
 pall
-push 4
+pop
+pall
+pop
+pall
+pop
 pall
--- monty.h
+++ monty.h
@@ -45,6 +45,7 @@ typedef struct instruction_s
 
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number __attribute__((unused)));
+void pop(stack_t **stack, unsigned int line_number);
 
 
 void arg_error(void);
@@ -53,7 +54,9 @@ void int_error(int line_count);
 void mem_error(void);
 void instruct_error(int line_count, char *instruct);
 
+
 void handle_instruction(FILE *file);
+void execute(instruction_t *op, char *instruct, char *num_arg, int line_count);
 
 
 void end_program(void);

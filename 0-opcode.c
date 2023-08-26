afd955364d0b12d132df22adaaa6c748a3b27790
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-08-26 03:12:35 -0700

    calculator functions

--- 0-opcode.c
+++ 0-opcode.c
@@ -71,12 +71,12 @@ void pint(stack_t **stack, unsigned int line_number)
  */
 void mul(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    (*stack)->next->n *= (*stack)->n;
-    pop(stack, line_number); // Remove the top element
+	(*stack)->next->n *= (*stack)->n;
+	pop(stack, line_number);
 }
